d592dd7950820bb2bb6687557ddf45f05501d2e5
KhajaNizamuddin.K@cognizant.com <KhajaNizamuddin.K@cognizant.com>
2020-11-24 23:32:50 -0800

    deleted old frameworks

--- craft-maven-cucumber-testng/src/main/java/com/cognizant/main/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.main;
-
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-
-}
--- craft-maven-modular-testng/src/test/java/allocator/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Package containing core runner files of CRAFT
- * 
- * @author Cognizant
- */
-package allocator;
